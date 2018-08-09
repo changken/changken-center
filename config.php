e23f5cd3b7fb29e5553994b2e53716a2fd68e8ff
changken <pp24513@gmail.com>
2018-08-09 20:05:11 +0800

    fix some problem
    same as title

--- config.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-define("DB_TYPE","mysql");
-define("DB_HOSTNAME","localhost");
-define("DB_NAME","member");
-define("DB_USERNAME","root");
-define("DB_PASSWORD","8abcd40000");
-define("DB_CHARSET","utf8");
